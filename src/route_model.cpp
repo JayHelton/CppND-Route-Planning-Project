8cb86f054d7fa80e16709afaafa1af91540ee8aa
markusbuchholz <markus.x.buchholz@gmail.com>
2019-08-16 00:23:33 +0200

    update files after debugging and testin. First submit

--- src/route_model.cpp
+++ src/route_model.cpp
@@ -1,6 +1,10 @@
 #include "route_model.h"
 #include <iostream>
-//ok
+
+/*
+Class constructor
+*/
+
 RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
     int counter = 0;
     for(Model::Node node : this->Nodes()) {
@@ -10,14 +14,18 @@ RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
     CreateNodeToRoadHashMap();
 };
 
-//ok
+/*
+Heuristic fubctio in A* alghoritm. Computation of euclidecian length between nodes
+*/
 float RouteModel::Node::distance(Node dest) const {
     return std::sqrt(std::pow((x - dest.x), 2) + std::pow((y - dest.y), 2));
 }
 
+/*
+Cretion of hash table. Linking the nodes with roads
+*/
 void RouteModel::CreateNodeToRoadHashMap()
 {
-
 
 	//std::cout<<"function START ::"<<__FUNCTION__<<std::endl;
 	for(const Model::Road& road : Roads()){
@@ -36,10 +44,13 @@ void RouteModel::CreateNodeToRoadHashMap()
     //std::cout<<"function STOP ::"<<__FUNCTION__<<std::endl;
 };
 
+/*
+Searching for the node naighbor
+*/
 
 RouteModel::Node * RouteModel::Node::FindNeighbor(std::vector<int> node_indices)
 {
-	std::cout<<"function START ::"<<__FUNCTION__<<std::endl;
+//	std::cout<<"function START ::"<<__FUNCTION__<<std::endl;
 	RouteModel::Node *closest_node = nullptr;
 	Node node;
 
@@ -51,31 +62,39 @@ RouteModel::Node * RouteModel::Node::FindNeighbor(std::vector<int> node_indices)
 			if(closest_node == nullptr || this->distance(node) < this->distance(*closest_node))
 				closest_node = &parent_model->SNodes()[node_index];
 	}
-std::cout<<"function STOP ::"<<__FUNCTION__<<std::endl;
+//std::cout<<"function STOP ::"<<__FUNCTION__<<std::endl;
 	return closest_node;
 };
 
+/*
+Fiding the closest neighbor from each roadthe current node this belonges to
+*/
 void RouteModel::Node::FindNeighbors(){
-	std::cout<<"function START ::"<<__FUNCTION__<<std::endl;
+//	std::cout<<"function START ::"<<__FUNCTION__<<std::endl;
 	for(auto &road : parent_model->node_to_road[this->index])
 	{
 		RouteModel::Node * new_neighbor = this->FindNeighbor(parent_model->Ways()[road->way].nodes);
 
 		if(new_neighbor) this->neighbors.emplace_back(new_neighbor);
 	}
-	std::cout<<"function STOP ::"<<__FUNCTION__<<std::endl;
+//	std::cout<<"function STOP ::"<<__FUNCTION__<<std::endl;
 	return;
 }
 
 
+/*
+The function finds the node int RouteModel that are closest to the starting and ending coordinates
+givrn by the user.
+*/
 
 RouteModel::Node& RouteModel::FindClosestNode(float x, float y) {
+    //	std::cout<<"function START ::"<<__FUNCTION__<<std::endl;
     RouteModel::Node input;
     input.x = x;
     input.y = y;
 
-    float min_dis = std::numeric_limits<float>::max();
-    float dis;
+    float min_distance = std::numeric_limits<float>::max();
+    float distance;
     int closest_idx;
 
     std::cout << "Moving into for loop to find closest node\n";
@@ -83,12 +102,10 @@ RouteModel::Node& RouteModel::FindClosestNode(float x, float y) {
     for(const Model::Road& road : Roads()) {
         if(road.type != Model::Road::Type::Footway) {
             for(int node_idx : Ways()[road.way].nodes) {
-                std::cout << "Calculating distance\n";
-                dis = input.distance(SNodes()[node_idx]);
-                if(dis < min_dis) {
-                    std::cout << "updating closest_idx\n";
+                distance = input.distance(SNodes()[node_idx]);
+                if(distance < min_distance) {
                     closest_idx = node_idx;
-                    min_dis = dis;
+                    min_distance = distance;
                 }
             }
         }
--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -2,9 +2,13 @@
 #include "route_planner.h"
 #include <algorithm>
 
+/*
+Class constructor
+*/
+
 RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
-//Convert inputs to percentege
-std::cout<<"function START ::"<<__FUNCTION__<<std::endl;
+
+//std::cout<<"function START ::"<<__FUNCTION__<<std::endl;
 start_x *= 0.01;
 start_y *= 0.01;
 end_x *= 0.01;
@@ -13,12 +17,11 @@ end_y *= 0.01;
 
 start_node = &m_Model.FindClosestNode(start_x, start_y);
 end_node = &m_Model.FindClosestNode(end_x, end_y);
-std::cout<<"function STOP ::"<<__FUNCTION__<<std::endl;
+//std::cout<<"function STOP ::"<<__FUNCTION__<<std::endl;
 };
 
 std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node *current_node){
-//create path found vector
-std::cout<<"function START ::"<<__FUNCTION__<<std::endl;
+//std::cout<<"function START ::"<<__FUNCTION__<<std::endl;
 distance = 0.0f;
 std::vector<RouteModel::Node> path_found;
 RouteModel::Node parent;
@@ -32,75 +35,72 @@ while (current_node -> parent !=nullptr){
 }
 path_found.push_back(*current_node);
 distance *=m_Model.MetricScale();
-std::cout<<"function STOP ::"<<__FUNCTION__<<std::endl;
+//std::cout<<"function STOP ::"<<__FUNCTION__<<std::endl;
 return path_found;
 
 };
 
-
-
-/////////////
-
+/*
+Perform A* alghorithm
+*/
 
 void RoutePlanner::AStarSearch(){
-std::cout<<"function START ::"<<__FUNCTION__<<std::endl;
-//initialize open_list with starting node.
+//std::cout<<"function START ::"<<__FUNCTION__<<std::endl;
 start_node ->visited =true;
 open_list.push_back(start_node);
 RouteModel::Node *current_node = nullptr;
 
-// Expand nodes until the goal is reached. Use heuristic to proritize what mode to open first.
-
 while(open_list.size()>0) {
-
-    // Selct the best node to expolore next
 
     current_node = NextNode();
 
-    //Check if the node selcted is the goal.
     if (current_node ->distance(*end_node) == 0){
 
-        //set the model path variable with the path found
         
-        m_Model.path =ConstructFinalPath(current_node);
+        m_Model.path = ConstructFinalPath(current_node);
         return;
     }
     AddNeighbors(current_node); 
 }
-std::cout<<"function STOP ::"<<__FUNCTION__<<std::endl;
+//std::cout<<"function STOP ::"<<__FUNCTION__<<std::endl;
 };
 
-
+/*
+Compute H value for the A* alghoritm function
+*/
 
 float RoutePlanner::CalculateHValue(RouteModel::Node *node){
-std::cout<<"function START / STOP::"<<__FUNCTION__<<std::endl;
+//std::cout<<"function START / STOP::"<<__FUNCTION__<<std::endl;
     return node->distance(*end_node);
 };
 
+/*
+Method sorts the list of open nifes in the A* sfearch and returns the node with lowest f-value, 
+and remove the node from the list of open nodes
+*/
 RouteModel::Node* RoutePlanner::NextNode(){
-std::cout<<"function START ::"<<__FUNCTION__<<std::endl;
+//std::cout<<"function START ::"<<__FUNCTION__<<std::endl;
     std::sort(open_list.begin(), open_list.end(), [](const auto &_1st, const auto &_2nd){
         return _1st->h_value + _1st->g_value < _2nd->h_value +_2nd->g_value;
     });
 
     RouteModel::Node *lowest_node = open_list.front();
     open_list.erase(open_list.begin());
-    std::cout<<"function STOP ::"<<__FUNCTION__<<std::endl;
+   // std::cout<<"function STOP ::"<<__FUNCTION__<<std::endl;
     return lowest_node;
 };
 
+/*
+Method add modes the search
+*/
 void RoutePlanner::AddNeighbors(RouteModel::Node * current_node){
-std::cout<<"function START ::"<<__FUNCTION__<<std::endl;
-// Expand the current mmode (add all unvisted neighbors to the open list)
+//std::cout<<"function START ::"<<__FUNCTION__<<std::endl;
 current_node->FindNeighbors();
 
 for(auto neighbor : current_node->neighbors){
     neighbor->parent = current_node;
     neighbor ->g_value = current_node->g_value + current_node->distance(*neighbor);
     neighbor-> h_value = CalculateHValue(neighbor);
-
-
-    //Add the neighbor to the open list.
     open_list.push_back(neighbor);
     neighbor->visited = true;
 
